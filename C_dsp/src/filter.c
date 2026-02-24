f4ff8a275cd306a2897a3dda4c0599add6b2195f
Accitri <preb.rasa@gmail.com>
2026-02-24 11:27:50 +0000

    updated

--- C_dsp/src/filter.c
+++ C_dsp/src/filter.c
@@ -1,162 +1,252 @@
 #include "filter.h"
-#include <math.h>
+#include "util.h"
 #include <stdio.h>
-//#include <unistd.h>
 
 
-void print_statistics(float32_t *p_y1, float32_t *p_y2, uint32_t p_y_len, uint32_t p_print_start, uint32_t p_print_n)
+void filter_signal(const float32_t *p_input, uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output)
 {
-    uint32_t p_print_end = (p_print_start + p_print_n < p_y_len) ? (p_print_start + p_print_n) : p_y_len;
+    const uint32_t N = p_filter->coeff_b_len;
+    const float32_t *h = p_filter->coeff_b_ptr;
 
-    printf("| Index |      y1      |      y2      |   Difference  |\n");
-    printf("|-------|--------------|--------------|----------------|\n");
-    uint32_t i;
-    for(i = p_print_start; i < p_print_end; i++)
-    {
-        printf("| %3d | %12.6f | %12.6f | %12.6f |\n", i, p_y1[i], p_y2[i], fabsf(p_y1[i] - p_y2[i]));
-    }
-
-    // Statistics summary
-    float32_t mean_y1 = 0, mean_y2 = 0, mean_diff = 0, total_diff = 0;
-    for(i = 0; i < p_y_len; i++)
-    {
-        mean_y1 += p_y1[i];
-        mean_y2 += p_y2[i];
-        total_diff += fabsf(p_y1[i] - p_y2[i]);
-    }
-    mean_y1 /= p_y_len;
-    mean_y2 /= p_y_len;
-    mean_diff = total_diff / p_y_len;
-    printf("Statistics Summary:\n");
-    printf("Mean of y1: %f\n", mean_y1);
-    printf("Mean of y2: %f\n", mean_y2);
-    printf("Mean of Differences: %f\n", mean_diff);
-    printf("Total of Differences: %f\n", total_diff);
-
-}
-
-void calculate_mean(uint32_t *p_input, uint32_t p_input_len, float32_t *p_output)
-{
-    uint32_t l_sum = 0;
-
-    uint32_t i;
-    for(i = 0; i < p_input_len; i++)
-    {
-        l_sum += p_input[i];
-    }
-
-    *p_output = (float32_t) l_sum / p_input_len;
-}
-
-
-void generate_signal(uint32_t *p_input, uint32_t p_input_len, float32_t *p_output, uint32_t p_output_len)
-{
-    float32_t l_mean = 0;
-    calculate_mean(p_input, p_input_len, &l_mean);
-
-    uint32_t l_repetitions = p_output_len / p_input_len;
-
-    uint32_t i;
-    for(i = 0; i < l_repetitions; i++)
-    {
-        uint32_t j;
-        uint32_t l_ith = (uint32_t)i * p_input_len;
-        for(j = 0; j < p_input_len; j++)
-        {
-            p_output[l_ith + j] = (float32_t) p_input[j] - l_mean;
-            // printf("x1[%d] = %f \n", l_ith + j, p_output[l_ith + j]);
-        }
-    }
-}
-
-
-void filter_signal(float32_t *p_input, uint32_t p_input_len, FIR_filter_t *p_filter, float32_t *p_output)
-{
-    uint32_t N = p_filter->coeff_b_len;
-    float32_t *h = p_filter->coeff_b_ptr;
-
-    uint32_t n;
+    // filter loop
+    uint32_t n, k;
     for (n = 0; n < p_input_len; n++)
     {
         float32_t l_acc = 0.0f;
 
-        uint32_t k;
+        // calculate tap-sums
         for (k = 0; k < N; k++) {
             if (n >= k) {
                 l_acc += p_input[n - k] * h[k];
             }
             else break;
         }
-
         p_output[n] = l_acc;
     }
 }
 
 
-void symm_filter_signal(float32_t *p_input, uint32_t p_input_len, FIR_filter_t *p_filter, float32_t *p_output) 
+void convolution(const int32_t *p_input1, uint32_t p_input1_len, const int32_t *p_input2, uint32_t p_input2_len, int32_t *p_output, uint32_t *p_output_len)
 {
-    uint32_t N = p_filter->coeff_b_len;
-    float32_t *h = p_filter->coeff_b_ptr;
-    bool l_is_odd = (N % 2 != 0);
+    *p_output_len = p_input1_len + p_input2_len - 1;
 
-    uint32_t l_half_len = (N / 2);
+    // convolution loop
+    uint32_t n, k;
+    for (n = 0; n < *p_output_len; n++)
+    {
+        uint32_t l_acc = 0;
+
+        // calculate tap-sums
+        for (k = 0; k < p_input1_len; k++)
+        {
+            if (n >= k && (n - k) < p_input2_len)
+            {
+                l_acc += p_input1[k] * p_input2[n - k];
+            }
+        }
+        p_output[n] = l_acc;
+    }
+}
+
+
+void symm_filter_signal(const float32_t *p_input, uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output) 
+{
+    const uint32_t N = p_filter->coeff_b_len;
+    const float32_t *h = p_filter->coeff_b_ptr;
+
+    const uint32_t l_half_len = (N / 2);
     
-    uint32_t n;
+    // filter loop
+    uint32_t n, k;
     for (n = 0; n < p_input_len; n++)
     {
         float32_t l_acc = 0.0f;
         
-        // Loop through the first half of coefficients
-        uint32_t k;
+        // loop through the first half of coefficients
         for (k = 0; k < l_half_len; k++) {
-            // Add symmetric taps before multiplying
+
+            // add symmetric taps before multiplying
             float32_t tap_sum = 0.0f;
             if (n >= k) 
             {
                 tap_sum += p_input[n - k];
             }
-            if (n >= (N - 1 - k)) 
+            if (n >= (N - 1 - k))
             {
                 tap_sum += p_input[n - (N - 1 - k)];
             }
-            
+            // calculate tap-sums
             l_acc += tap_sum * h[k];
         }
         
-        // Handle the middle tap if N is odd
-        if (l_is_odd) {
-            if (n >= l_half_len) {
-                l_acc += p_input[n - l_half_len] * h[l_half_len];
-            }
+        // handle the middle tap if N is odd (assumed filter has odd length)
+        if (n >= l_half_len)
+        {
+            l_acc += p_input[n - l_half_len] * h[l_half_len];
         }
-
         p_output[n] = l_acc;
     }
 }
 
 
-
-void record_output(float32_t *p_output, uint32_t p_output_len, const char *p_filename)
+static void symm_filter_signal_short_nconv(const int16_t *x, const uint32_t p_input_len, const uint32_t N, const int16_t *h, int32_t *y, const int16_t p_coeff_norm)
 {
-    FILE *l_file = fopen(p_filename, "w");
+    const uint32_t l_half_len = (N / 2);
 
-    if (l_file == NULL) 
+    // filter loop
+    uint32_t n, k;
+    for (n = 0; n < p_input_len; n++)
     {
-        printf("Error. Not able to open file %s for writing.\n", p_filename);
-        return;
+        int32_t l_acc = 0;
+
+        // loop through the first half of coefficients
+        for (k = 0; k < l_half_len; k++) {
+
+            // add symmetric taps before multiplying
+            int32_t tap_sum = 0;
+            if (n >= k)
+            {
+                tap_sum += x[n - k];
+            }
+            if (n >= (N - 1 - k))
+            {
+                tap_sum += x[n - (N - 1 - k)];
+            }
+            // calculate tap-sums
+            l_acc += tap_sum * h[k];
+        }
+
+        // handle the middle tap if N is odd (assumed filter has odd length)
+        if (n >= l_half_len) {
+            l_acc += (int32_t)x[n - l_half_len] * h[l_half_len];
+        }
+
+        // rounding
+        // add half-LSB (1 << (s - 1)) to round to nearest integer during shift
+        l_acc += (1 << (p_coeff_norm - 1));
+
+        // re-normalization
+        l_acc >>= p_coeff_norm;
+
+        y[n] = l_acc;
     }
+}
+
+
+void symm_filter_signal_short(const float32_t *p_input, const uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output, const float32_t p_coeff_norm)
+{
+    const uint32_t N = p_filter->coeff_b_len;
+
+    // convert coefficients to int
+    static int16_t h_q[BUFF_SIZE] = { 0 };
+    float_to_int16_normalized(p_filter->coeff_b_ptr, N, h_q, p_coeff_norm);
+
+    // convert input to int
+    static int16_t x_q[BUFF_SIZE] = { 0 };
+    float_to_int16_normalized(p_input, p_input_len, x_q, p_coeff_norm);
+
+    static int32_t y_q[BUFF_SIZE] = { 0 };
+    // filter loop
+    symm_filter_signal_short_nconv(x_q, p_input_len, N, h_q, y_q, 1);
+
+    // convert output to float
+    int32_to_float(y_q, p_input_len, p_output, 1, p_coeff_norm);
+}
+
+
+
+void fir2_set_run(const float32_t *p_input, const uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output, const float32_t p_coeff_norm)
+{
+    const uint32_t N = p_filter->coeff_b_len;
+    const uint32_t M = p_input_len;
+    const uint32_t L_even = (N % 2U == 0U) ? N : (N + 1U);
+    const uint32_t M_even = (M % 2U == 0U) ? M : (M + 1U);
+
+    // right-shift used by fir2() before storing to int16.
+    //      Q15xQ15 -> Q30 accumulation -> Q15 output.
+    const int s = 15;
+
+    // convert coefficients
+    static int16_t h_q[BUFF_SIZE] = { 0 };
+    float_to_int16_normalized(p_filter->coeff_b_ptr, N, h_q, p_coeff_norm);
+
+    // convert input
+    static int16_t x_q[BUFF_SIZE] = { 0 };
+    float_to_int16_normalized(p_input, M, x_q, p_coeff_norm);
+
+    // build padded input of length (L_even + M_even) samples
+    const uint32_t x_pad_len = L_even + M_even ;
+    static int16_t x_pad[BUFF_ZERO_SIZE] = { 0 };
 
     uint32_t i;
-    for (i = 0; i < p_output_len; i++)
+    for(i = 0; i < (L_even - 1U); i++)
     {
-        fprintf(l_file, "%f,", p_output[i]);
+        x_pad[i] = 0;
     }
-    
-    // remove trailing comma
-    fseek(l_file, -1, SEEK_END);
-//    ftruncate(fileno(l_file), ftell(l_file));
+    for(i = 0; i < M; i++)
+    {
+        x_pad[(L_even - 1U) + i] = x_q[i];
+    }
+    for(i = (L_even - 1U) + M; i < x_pad_len; i++)
+    {
+        x_pad[i] = 0;
+    }
 
-    fclose(l_file);
+    // build padded and reversed coefficient array
+    static int16_t h_pad[BUFF_ZERO_SIZE] = { 0 };
+    for(i = 0; i < N; i++)
+    {
+        h_pad[(L_even - 1U) - i] = h_q[i];
+    }
+    for(i = N; i < L_even; i++)
+    {
+        h_pad[(L_even - 1U) - i] = 0;
+    }
+
+    static int16_t y_q[BUFF_ZERO_SIZE + 300] = { 0 };
+
+    // ensure 4-byte word alignment
+    _nassert(((int)(x_pad) & 0x3) == 0);
+    _nassert(((int)(h_pad) & 0x3) == 0);
+    _nassert(((int)(y_q) & 0x3) == 0);
+
+    // filter loop
+    fir2((const int *)x_pad, (const int *)h_pad, y_q, (int)L_even, (int)M_even, s);
+
+
+    // x and h were both scaled by p_coeff_norm => scaled by p_coeff_norm^2.
+    // fir2 stores (acc >> s) => reconstruct by multiplying by 2^s.
+    const float32_t inv_scale = (float32_t)(1U << s) / (p_coeff_norm * p_coeff_norm);
+
+    for(i = 0; i < M; i++)
+    {
+        p_output[i] = (float32_t)y_q[i] * inv_scale;
+    }
 }
+
+// extracted from Example 3–14. FIR Filter— Optimized Form in C6x programmer's guide(198d)
+void fir2(const int x[], const int h[], short y[], int n, int m, int s)
+{
+    int i, j;
+    long y0, y1;
+    long round = 1L << (s - 1);
+
+    #pragma MUST_ITERATE (,,2);
+    for (j = 0; j < (m >> 1); j++)
+    {
+        y0 = y1 = round;
+        #pragma MUST_ITERATE (,,2);
+        for (i = 0; i < (n >> 1); i++)
+        {
+            y0 += _mpy (x[i + j], h[i]);
+            y0 += _mpyh (x[i + j], h[i]);
+            y1 += _mpyhl(x[i + j], h[i]);
+            y1 += _mpylh(x[i + j + 1], h[i]);
+        }
+        *y++ = (int)(y0 >> s);
+        *y++ = (int)(y1 >> s);
+    }
+}
+
 
 
--- C_dsp/src/main.c
+++ C_dsp/src/main.c
@@ -1,60 +1,158 @@
-
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include "util.h"
 #include "filter.h"
 #include "data.h"
+#include "data_1_fixed.h"
+#include "data_2_fixed.h"
 
-#define DATA_FILE_1 "/home/preben/Documents/ee580_dsp/data1.txt"
-#define DATA_FILE_2 "/home/preben/Documents/ee580_dsp/data2.txt"
 
+/* DEFINES */
+// file paths for data outputs
+#define DATA_DIR "/home/preben/Documents/ee580_dsp/EE580-Filtering-Assignment/C_dsp/data/"
+#define DATA_FILE_1 DATA_DIR "data1.txt"
+#define DATA_FILE_2 DATA_DIR "data2.txt"
+
+// signal generation declarations
 #define REG_LENGTH    9U
-#define BUFF_SIZE     900U
 #define REG1_LAST4    8874U
 #define REG2_LAST4    4642U
 
+// convolution buffer
+#define SAMPLE_LEN 4
+
+
+/* GLOBAL DECLARATIONS */
+static uint32_t g_reg1[REG_LENGTH] = { 2, 0, 2, 1, 1, 8, 8, 7, 4 }; // 202118874
+static uint32_t g_reg2[REG_LENGTH] = { 2, 0, 2, 1, 1, 4, 6, 4, 2 }; // 202114642
+
+// buffers for data input and output
+#pragma DATA_ALIGN(g_x, 8); // Align to 8-byte (64-bit) boundary
+static float32_t g_x[BUFF_ZERO_SIZE] = { 0 }; // stack overflow if not static
+#pragma DATA_ALIGN(g_y, 8); // Align to 8-byte (64-bit) boundary
+static float32_t g_y[BUFF_ZERO_SIZE] = { 0 };
+
+
+/* STATIC DECLARATIONS */
+static void profile_filter_implementations(const float32_t *p_input, const uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output);
+
+static double ticks_to_seconds(clock_t p_start, clock_t p_end)
+{
+    return (double) (p_end - p_start) / CLOCKS_PER_SEC;
+}
+
+
 /**
  * main.c
+ * - declare FIR filter structs
+ * - filter signal x1
+ * - filter signal x2
+ * - profile various FIR filter implementations
  */
 int main(void)
 {
-    uint32_t l_reg1[REG_LENGTH] = { 2, 0, 2, 1, 1, 8, 8, 7, 4 }; // 202118874
-    uint32_t l_reg2[REG_LENGTH] = { 2, 0, 2, 1, 1, 4, 6, 4, 2 }; // 202114642
+    TSCL = 0;   // this starts the clock
 
-    float32_t l_x1[BUFF_SIZE] = { 0 };
-    float32_t l_x2[BUFF_SIZE] = { 0 };
-    float32_t l_y1[BUFF_SIZE] = { 0 };
-    float32_t l_y2[BUFF_SIZE] = { 0 };
+    filter_t l_FIR_1;
+    l_FIR_1.symmetric = true;
+    l_FIR_1.coeff_b_len = Filter_1_Fixed_N_FIR_B;
+    l_FIR_1.coeff_a_len = Filter_1_Fixed_N_FIR_A;
+    l_FIR_1.coeff_a_ptr = Filter_1_Fixed_a_fir;
+    l_FIR_1.coeff_b_ptr = Filter_1_Fixed_b_fir;
 
-    FIR_filter_t g_FIR_1;
-    g_FIR_1.symmetric = true;
-    g_FIR_1.coeff_b_len = Filter_1_N_FIR_B;
-    g_FIR_1.coeff_a_len = Filter_1_N_FIR_A;
-    g_FIR_1.coeff_a_ptr = Filter_1_a_fir;
-    g_FIR_1.coeff_b_ptr = Filter_1_b_fir;
+    filter_t l_FIR_2;
+    l_FIR_2.symmetric = true;
+    l_FIR_2.coeff_b_len = Filter_2_Fixed_N_FIR_B;
+    l_FIR_2.coeff_a_len = Filter_2_Fixed_N_FIR_A;
+    l_FIR_2.coeff_a_ptr = Filter_2_Fixed_a_fir;
+    l_FIR_2.coeff_b_ptr = Filter_2_Fixed_b_fir;
 
-    FIR_filter_t g_FIR_2;
-    g_FIR_2.symmetric = true;
-    g_FIR_2.coeff_b_len = Filter_2_N_FIR_B;
-    g_FIR_2.coeff_a_len = Filter_2_N_FIR_A;
-    g_FIR_2.coeff_a_ptr = Filter_2_a_fir;
-    g_FIR_2.coeff_b_ptr = Filter_2_b_fir;
+    // filter signal x1
+    generate_signal(g_reg1, REG_LENGTH, g_x, BUFF_SIZE);
+    filter_signal(g_x, BUFF_SIZE, &l_FIR_1, g_y);
+    record_output(g_y, BUFF_SIZE, DATA_FILE_1);
 
-    generate_signal(l_reg1, REG_LENGTH, l_x1, BUFF_SIZE);
-    generate_signal(l_reg2, REG_LENGTH, l_x2, BUFF_SIZE);
 
-    if(g_FIR_1.symmetric == true)
-        symm_filter_signal(l_x1, BUFF_SIZE, &g_FIR_1, l_y1);
-    else
-        filter_signal(l_x1, BUFF_SIZE, &g_FIR_1, l_y1);
+    // filter signal x2
+    generate_signal(g_reg2, REG_LENGTH, g_x, BUFF_SIZE);
+    filter_signal(g_x, BUFF_SIZE, &l_FIR_2, g_y);
+    record_output(g_y, BUFF_SIZE, DATA_FILE_2);
 
-    if(g_FIR_2.symmetric == true)
-        symm_filter_signal(l_x2, BUFF_SIZE, &g_FIR_2, l_y2);
-    else
-        filter_signal(l_x2, BUFF_SIZE, &g_FIR_2, l_y2);
+    // profile FIR implementations
+    profile_filter_implementations(g_x, BUFF_SIZE, &l_FIR_1, g_y);
 
-    record_output(l_y1, BUFF_SIZE, DATA_FILE_1);
-    record_output(l_y2, BUFF_SIZE, DATA_FILE_2);
+    // convolution of 4 integer samples signals
+    int32_t l_sample_signal1[SAMPLE_LEN] = { 1, 1, 1, 1};
+    int32_t l_sample_signal2[SAMPLE_LEN] = { 1, 2, 1, 2};
+    int32_t l_conv_output[SAMPLE_LEN * 2] = { 0 };
+    uint32_t l_conv_len = 0;
 
-//    print_statistics(l_y1, l_y2, BUFF_SIZE, 860, 50);
+    convolution(l_sample_signal1, SAMPLE_LEN, l_sample_signal2, SAMPLE_LEN, l_conv_output, &l_conv_len);
 
     return 0;
 }
+
+static void profile_filter_implementations(const float32_t *p_input, const uint32_t p_input_len, const filter_t *p_filter, float32_t *p_output)
+{
+    // CPU cycle profiling
+    clock_t start, stop, overhead;
+    start = clock();
+    stop = clock();
+    overhead = stop - start;
+
+    // number of output samples to print
+    const uint32_t l_samples_to_print = 10;
+
+    // calculation of scaling norm for fixed-point integer conversions
+    float32_t l_max = 0;
+    uint16_t i;
+    // find max sample value
+    for(i = 0; i < p_input_len; i++)
+    {
+        float32_t l_sample = abs(p_input[i]);
+        if(l_sample > l_max) l_max = l_sample;
+    }
+    // find max coefficient value
+    for(i = 0; i < p_filter->coeff_b_len; i++)
+    {
+        float32_t l_sample = abs(p_filter->coeff_b_ptr[i]);
+        if(l_sample > l_max) l_max = l_sample;
+    }
+    const float32_t l_scaled_norm = 32767.0f / (l_max);
+    printf("\nscaled norm = %f \n", l_scaled_norm);
+
+    // standard filter
+    start = clock();
+    filter_signal(p_input, p_input_len, p_filter, p_output);
+    stop = clock();
+    printf("\nfilter cycles: %d, seconds: %f", stop - start - overhead, ticks_to_seconds(start, stop - overhead ));
+    record_output(p_output, p_input_len, DATA_DIR "data_standard.txt");
+    print_results(p_output, l_samples_to_print);
+
+    // symmetric filter
+    start = clock();
+    symm_filter_signal(p_input, p_input_len, p_filter, p_output);
+    stop = clock();
+    printf("\nsymm cycles: %d, seconds: %f", stop - start - overhead, ticks_to_seconds(start, stop - overhead ));
+    record_output(p_output, p_input_len, DATA_DIR "data_symmetric.txt");
+    print_results(p_output, l_samples_to_print);
+
+    // symmetric filter with fixed-point
+    start = clock();
+    symm_filter_signal_short(p_input, p_input_len, p_filter, p_output, l_scaled_norm);
+    stop = clock();
+    printf("\nsymm short cycles: %d, seconds: %f", stop - start - overhead, ticks_to_seconds(start, stop - overhead ));
+    record_output(p_output, p_input_len, DATA_DIR "data_symmetric_fixed.txt");
+    print_results(p_output, l_samples_to_print);
+
+    // SIMD filter with fixed-point
+    start = clock();
+    fir2_set_run(p_input, p_input_len, p_filter, p_output, l_scaled_norm);
+    stop = clock();
+    printf("\nfir2 cycles: %d, seconds: %f", stop - start - overhead, ticks_to_seconds(start, stop - overhead ));
+    record_output(p_output, p_input_len, DATA_DIR "data_fixed_simd.txt");
+    print_results(p_output, l_samples_to_print);
+
+}
+
